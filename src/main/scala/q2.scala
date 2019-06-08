8ad07a948cddc4d19b7b7c7c792ddcf38cb901e0
damsarar <damsarar@gmail.com>
2019-06-09 03:17:02 +0530

    Initial commit

--- src/main/scala/q2.scala
+++ src/main/scala/q2.scala
@@ -1,25 +1,34 @@
 import scala.io.Source
 
 object q2 {
-  def main(args: Array[String]): Unit ={
-    var a:Map[Char,Int] = Map()
+  def main(args: Array[String]): Unit = {
+    var a: Map[Char, Int] = Map()
     val inputstring = "I love ucsc";
     var splitted = inputstring.split("[\\s+.]");
+    var maxno = 0;
+    var maxchar = ' ';
 
-    for(c <- splitted){
+    for (c <- splitted) {
       var letters = c.toList;
-      for(l <- letters){
-        if(a.contains(l)){
+      for (l <- letters) {
+        if (a.contains(l)) {
           var temp = a(l);
-          temp+=1;
-          a+=(l->temp);
-        }else{
-          a+=(l->1);
+          temp += 1;
+          a += (l -> temp);
+        } else {
+          a += (l -> 1);
         }
       }
     }
-    println(a);
 
+    a.keys.foreach( i => {
+      if(a(i)>maxno){
+        maxno = a(i);
+        maxchar = i;
+      }
+    })
+
+    println("Maximum occurring character: " + maxchar);
 
   }
 
